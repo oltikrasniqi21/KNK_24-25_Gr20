2d14d3a4e55b8f6b8e9e1143023967928acb5624
oltikrasniqi21 <oltkrasn@gmail.com>
2025-03-27 13:18:10 +0100

    Klasa DBConnector

--- src/main/java/Database/DBCustomConnector.java
+++ src/main/java/Database/DBCustomConnector.java
@@ -4,20 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-//lidhjen me databaze do e marrim te gatshme n
 public class DBCustomConnector {
     private static Connection connection = null;
     private static final String dbName = "Projekti-KNK";
     private static final String dbHost = "localhost";
-    private static final  String dbuser = "postgres";
-    private static final  String dbpassword = "1234";
+    private static final  String dbUser = "postgres";
+    private static final  String dbPassword = "1234";
     private static final  String dbString = "jdbc:postgresql://"+dbHost+"/"+dbName;
-
 
     public static Connection getConnection(){
         if(connection == null){
             try{
-                connection = DriverManager.getConnection(dbString, dbuser, dbpassword);
+                connection = DriverManager.getConnection(dbString, dbUser, dbPassword);
                 System.out.println("succesful");
             }catch(SQLException e){
                 e.printStackTrace();
