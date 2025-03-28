8d45bc266ecdbe74f4a5dbab8d985f7907bdc085
olsadomi <77245008+olsadomi@users.noreply.github.com>
2025-03-28 23:05:58 +0100

    added connections of repository to database using DBCUstomConnector's static method 'getConnection'

--- src/main/java/Repository/ApplicationsRepository.java
+++ src/main/java/Repository/ApplicationsRepository.java
@@ -5,9 +5,4 @@ import Database.DBCustomConnector;
 import java.sql.Connection;
 
 public class ApplicationsRepository {
-
-    private Connection connection;
-    public ApplicationsRepository() {
-        this.connection = DBCustomConnector.getConnection();
-    }
 }
--- src/main/java/Repository/ScholarshipsRepository.java
+++ src/main/java/Repository/ScholarshipsRepository.java
@@ -5,9 +5,4 @@ import Database.DBCustomConnector;
 import java.sql.Connection;
 
 public class ScholarshipsRepository {
-
-    private Connection connection;
-    public ScholarshipsRepository() {
-        this.connection = DBCustomConnector.getConnection();
-    }
 }
