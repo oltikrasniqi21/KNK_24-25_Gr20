9526fd1d85112904fa8d67adf31134ceb96b4d64
Rreze Ejupi <rrezeejupi@gmail.com>
2025-03-29 23:48:59 +0100

    created the tables for database

---
+++ tabelat.sql
@@ -0,0 +1,40 @@
+CREATE TABLE users(
+	user_id SERIAL PRIMARY KEY,
+	password VARCHAR(255) NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	role VARCHAR(10) NOT NULL CHECK (LOWER(role) IN ('student', 'admin'))
+);
+
+CREATE TABLE students(
+	student_id INTEGER PRIMARY KEY REFERENCES users(user_id) ON DELETE CASCADE,
+	gpa NUMERIC(3,2) NOT NULL CHECK(gpa BETWEEN 6.00 AND 10.00),
+	year_of_study INTEGER NOT NULL CHECK (year_of_study BETWEEN 1 AND 5),
+	university VARCHAR(50) NOT NULL,
+	faculty VARCHAR(50) NOT NULL,
+	major VARCHAR(50) NOT NULL,
+	courses_left INTEGER NOT NULL CHECK (courses_left >= 0),
+	priority VARCHAR(100) CHECK(priority IS NULL OR LOWER(priority) IN('veteran', 'disabled'))
+);
+
+
+CREATE TABLE scholarships(
+	scholarship_id SERIAL PRIMARY KEY,
+	scholarship_name VARCHAR(100) NOT NULL,
+	provider VARCHAR(50) NOT NULL,
+	amount INTEGER NOT NULL CHECK (amount >0),
+	deadline_date DATE NOT NULL,
+	required_gpa NUMERIC(3,2) CHECK (required_gpa IS NULL OR (required_gpa BETWEEN 6.00 AND 10.00)),
+	required_year INTEGER CHECK (required_year IS NULL OR (required_year BETWEEN 1 AND 5)),
+	required_major VARCHAR(50)
+);
+
+
+CREATE TABLE applications(
+	application_id SERIAL PRIMARY KEY,
+	student_id INTEGER NOT NULL REFERENCES students(student_id) ON DELETE CASCADE,
+	scholarship_id INTEGER NOT NULL REFERENCES scholarships(scholarship_id) ON DELETE CASCADE,
+	application_date DATE NOT NULL DEFAULT CURRENT_DATE,
+	status VARCHAR(10) NOT NULL CHECK (LOWER(status) IN ('pending', 'approved', 'rejected'))
+);
