6a7688244a5b489cec21c1dd6c3dce4d2357d598
olsadomi <77245008+olsadomi@users.noreply.github.com>
2025-04-02 14:51:17 +0200

    added method getall in Users' repository

--- src/main/java/Repository/UsersRepository.java
+++ src/main/java/Repository/UsersRepository.java
@@ -1,14 +1,37 @@
 package Repository;
 
 import Database.DBCustomConnector;
+import models.Users;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class UsersRepository {
 
     private Connection connection;
     public UsersRepository() {
         this.connection = DBCustomConnector.getConnection();
+    }
+
+    public ArrayList<Users> getAll() {
+        String query = "SELECT * FROM USERS";
+        ArrayList<Users> users = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                users.add(Users.getInstance(resultSet));
+            }
+            return users;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return users;
+        }
     }
 
 }
