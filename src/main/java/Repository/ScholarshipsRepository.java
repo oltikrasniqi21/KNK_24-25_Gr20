6d74d15a9a9aac0ca595dfb1cb952a23bda9ca4e
oltikrasniqi21 <oltkrasn@gmail.com>
2025-04-04 08:23:59 +0200

    krijimi getAll()

--- src/main/java/Repository/ScholarshipsRepository.java
+++ src/main/java/Repository/ScholarshipsRepository.java
@@ -1,8 +1,38 @@
 package Repository;
 
 import Database.DBCustomConnector;
+import models.Scholarships;
 
+import javax.xml.transform.Result;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class ScholarshipsRepository {
+    private Connection connection;
+
+    public ScholarshipsRepository(){
+        this.connection = DBCustomConnector.getConnection();
+    }
+
+    public ArrayList<Scholarships> getAll(){
+        ArrayList<Scholarships> scholarships = new ArrayList<>();
+        String query = "SELECT * FROM SCHOLARSHIPS";
+
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while(resultSet.next()){
+                Scholarships scholarship = Scholarships.getInstance(ResultSet);
+                scholarships.add(scholarship);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return scholarships;
+    }
 }
