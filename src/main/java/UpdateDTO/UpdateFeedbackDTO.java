e1aea5c329c4378167119736fdc39b76dff43bc2
olsadomi <77245008+olsadomi@users.noreply.github.com>
2025-04-25 16:04:30 +0200

    created feedback repository class. modified UpdateFeedbackDTO, added feedbackId field

--- src/main/java/UpdateDTO/UpdateFeedbackDTO.java
+++ src/main/java/UpdateDTO/UpdateFeedbackDTO.java
@@ -1,10 +1,20 @@
 package UpdateDTO;
 
 public class UpdateFeedbackDTO {
+    private int feedbackId;
     private String response;
 
-    public UpdateFeedbackDTO(String response) {
+    public UpdateFeedbackDTO(int feedbackId, String response) {
+        this.feedbackId = feedbackId;
         this.response = response;
+    }
+
+    public int getFeedbackId() {
+        return feedbackId;
+    }
+
+    public void setFeedbackId(int feedbackId) {
+        this.feedbackId = feedbackId;
     }
 
     public String getResponse() {
