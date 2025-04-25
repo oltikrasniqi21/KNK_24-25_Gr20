94310c988175a9bcf03d1bf68db64bf406a4f245
olsadomi <77245008+olsadomi@users.noreply.github.com>
2025-04-25 16:06:59 +0200

    modified UpdateFaqDTO, added faqId field

--- src/main/java/UpdateDTO/UpdateFaqDTO.java
+++ src/main/java/UpdateDTO/UpdateFaqDTO.java
@@ -1,12 +1,22 @@
 package UpdateDTO;
 
 public class UpdateFaqDTO {
+    private int faqId;
     private String question;
     private String answer;
 
-    public UpdateFaqDTO(String question, String answer) {
+    public UpdateFaqDTO(int faqId, String question, String answer) {
+        this.faqId = faqId;
         this.question = question;
         this.answer = answer;
+    }
+
+    public int getFaqId() {
+        return faqId;
+    }
+
+    public void setFaqId(int faqId) {
+        this.faqId = faqId;
     }
 
     public String getQuestion() {
