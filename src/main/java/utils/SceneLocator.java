09f41dea216d298d0577ff663f583e4977869b6b
olsadomi <77245008+olsadomi@users.noreply.github.com>
2025-05-13 01:36:54 +0200

    created feedback.fxml... modified multilang, SceneLocator and AdminHomePageController connections with buttons

--- src/main/java/utils/SceneLocator.java
+++ src/main/java/utils/SceneLocator.java
@@ -9,7 +9,7 @@ public class SceneLocator {
     public static final String MANAGE_USERS_PAGE = "/views/manage_users.fxml";
     public static final String MANAGE_SCHOLARSHIPS_PAGE = "/views/manage_scholarships.fxml";
     public static final String MANAGE_APPLICATIONS_PAGE = "/views/manage_applications.fxml";
-    public static final String MANAGE_FEEDBACK_PAGE = "/views/manage_feedback.fxml";
     public static final String MANAGE_ADMIN_NOTIFICATION_PAGE = "/views/admin_notification.fxml";
     public static final String MANAGE_FAQ_PAGE = "/views/faq.fxml";
+    public static final String MANAGE_FEEDBACK_PAGE = "/views/feedback.fxml";
 }
