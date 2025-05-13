e681d2b6f5a2783732db1f7c5e759aab7b5c0710
olsadomi <77245008+olsadomi@users.noreply.github.com>
2025-05-13 19:05:40 +0200

    modified CreateNotificationDTO

--- src/main/java/CreateDTO/CreateNotificationDTO.java
+++ src/main/java/CreateDTO/CreateNotificationDTO.java
@@ -7,12 +7,14 @@ public class CreateNotificationDTO {
     private String message;
     private Timestamp created_at;
     private boolean read_status;
+    private boolean is_broadcast;
 
-    public CreateNotificationDTO(int student_id, String message, Timestamp created_at, boolean read_status) {
+    public CreateNotificationDTO(int student_id, String message, Timestamp created_at, boolean read_status, boolean is_broadcast) {
         this.student_id = student_id;
         this.message = message;
         this.created_at = created_at;
         this.read_status = read_status;
+        this.is_broadcast = is_broadcast;
     }
 
     public int getStudent_id() {
@@ -29,5 +31,9 @@ public class CreateNotificationDTO {
 
     public boolean isRead_status() {
         return read_status;
+    }
+
+    public boolean is_broadcast() {
+        return is_broadcast;
     }
 }
