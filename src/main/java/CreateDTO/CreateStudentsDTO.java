b95854b08fefc50e9f0b56aac75fa03307a80a55
RigaZubaku <riga.zubaku@student.uni-pr.edu>
2025-05-14 14:35:44 +0200

    refactor: change GPA field type from double to Double

--- src/main/java/CreateDTO/CreateStudentsDTO.java
+++ src/main/java/CreateDTO/CreateStudentsDTO.java
@@ -10,7 +10,6 @@ public class CreateStudentsDTO {
     private String university;
     private String faculty;
     private String major;
-    //private Integer coursesLeft;
     private String priority;
 
     public CreateStudentsDTO(String firstName, String lastName, String email, String password, Double gpa, Integer yearOfStudy, String university, String faculty, String major, String priority) {
@@ -23,7 +22,6 @@ public class CreateStudentsDTO {
         this.university = university;
         this.faculty = faculty;
         this.major = major;
-       // this.coursesLeft = coursesLeft;
         this.priority = priority;
     }
 
@@ -98,14 +96,6 @@ public class CreateStudentsDTO {
     public void setMajor(String major) {
         this.major = major;
     }
-
-    //public Integer getCoursesLeft() {
-       // return coursesLeft;
-    //}
-
-   // public void setCoursesLeft(Integer coursesLeft) {
-        //this.coursesLeft = coursesLeft;
-   // }
 
     public String getPriority() {
         return priority;
--- src/main/java/UpdateDTO/UpdateStudentsDTO.java
+++ src/main/java/UpdateDTO/UpdateStudentsDTO.java
@@ -2,23 +2,21 @@ package UpdateDTO;
 
 public class UpdateStudentsDTO {
 
-    private double gpa;
+    private Double gpa;
     private Integer yearOfStudy;    //perdorim Integer e jo int, sepse nese nuk caktojme nje vlere te nje kolone, default mos me qene zero '0', por NULL. E tipi 'int' nuk e vendos NULL.
-    //private Integer coursesLeft;
     private String priority;
 
-    public UpdateStudentsDTO(double gpa, Integer yearOfStudy, String priority) {
+    public UpdateStudentsDTO(Double gpa, Integer yearOfStudy, String priority) {
         this.gpa = gpa;
         this.yearOfStudy = yearOfStudy;
-        //this.coursesLeft = coursesLeft;
         this.priority = priority;
     }
 
-    public double getGpa() {
+    public Double getGpa() {
         return gpa;
     }
 
-    public void setGpa(double gpa) {
+    public void setGpa(Double gpa) {
         this.gpa = gpa;
     }
 
@@ -29,14 +27,6 @@ public class UpdateStudentsDTO {
     public void setYearOfStudy(Integer yearOfStudy) {
         this.yearOfStudy = yearOfStudy;
     }
-
-    //public Integer getCoursesLeft() {
-      //  return coursesLeft;
-    //}
-
-    //public void setCoursesLeft(Integer coursesLeft) {
-      //  this.coursesLeft = coursesLeft;
-    //}
 
     public String getPriority() {
         return priority;
