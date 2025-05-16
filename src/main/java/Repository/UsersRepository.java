cf15c3a260c51770836340a2317d5814f11e0c28
Valmir Mustafa <valmirmustafa2005@gmail.com>
2025-05-17 00:36:41 +0200

    Modified StudentListController,UserRepository,added filtering to StudentListController, improved multilanguage support for student_list.fxml

--- src/main/java/Repository/UsersRepository.java
+++ src/main/java/Repository/UsersRepository.java
@@ -1,10 +1,12 @@
 package Repository;
 
 import Database.DBCustomConnector;
+import models.Students;
 import models.Users;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class UsersRepository {
 
@@ -65,5 +67,51 @@ public class UsersRepository {
         return null;
     }
 
+public List<Students> getAllStudents() {
+    String query = "SELECT u.id, u.password, u.first_name, u.last_name, u.email, u.role, u.status, " +
+            "s.gpa, s.year_of_study, s.university, s.faculty, s.major, s.priority " +
+            "FROM users u JOIN students s ON u.id = s.id " +
+            "WHERE u.role = 'student'";
+    List<Students> students = new ArrayList<>();
+    try (Statement statement = connection.createStatement();
+         ResultSet resultSet = statement.executeQuery(query)) {
+        while (resultSet.next()) {
+            students.add(Students.getInstance(resultSet));
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    return students;
+}
+
+
+    public List<Students> searchStudents(String searchTerm) {
+        String query = "SELECT u.id, u.password, u.first_name, u.last_name, u.email, u.role, u.status, " +
+                "s.gpa, s.year_of_study, s.university, s.faculty, s.major, s.priority " +
+                "FROM users u JOIN students s ON u.id = s.id " +
+                "WHERE u.role = 'student' AND " +
+                "(LOWER(u.first_name) LIKE ? OR " +
+                "LOWER(u.last_name) LIKE ? OR " +
+                "LOWER(s.university) LIKE ? OR " +
+                "LOWER(s.faculty) LIKE ? OR " +
+                "LOWER(s.major) LIKE ?)";
+
+        List<Students> students = new ArrayList<>();
+        String likeTerm = "%" + searchTerm.toLowerCase() + "%";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            for (int i = 1; i <= 5; i++) {
+                preparedStatement.setString(i, likeTerm);
+            }
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                students.add(Students.getInstance(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return students;
+    }
 
 }
